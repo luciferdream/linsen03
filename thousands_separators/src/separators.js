fc3f34dd5701fd697d0572e5084e9d64b946f232
luciferdream <422512433@qq.com>
2016-05-25 00:43:32 +0800

    Update separators.js

--- thousands_separators/src/separators.js
+++ thousands_separators/src/separators.js
@@ -1,10 +1,23 @@
+'use strict';
+
 function thousands_separators(num) {
-  var parts;
-  if (!isNaN(parseFloat(num)) && isFinite(num)) {
-    num = Number(num);
-    num = num.toString();
-    parts = num.split('.');
-    parts[0] = parts[0].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + (','));
-    return parts.join('.');
-  }
+	var temp;
+  	if(num >= 1000 && num < 1000000){
+		temp = num / 1000;
+		temp = Math.floor(temp);
+		var temp2 = num;
+		while(temp2 >= 1000) temp2-=1000;
+		return temp + ',' + temp2;
+	}
+	if(num >= 1000000 && num < 1000000000){
+		temp = num / 1000000;
+		temp = Math.floor(temp);
+		var temp2 = num / 1000;
+		temp2 = Math.floor(temp2);
+		var temp3 = num-1000000;
+		while(temp3 >= 1000) temp3-=1000;
+		return temp + ',' + temp2 + ',' + temp3;
+	}
 }
+
+module.exports = thousands_separators;
