63bfdff506d910f55d135bdbca127e340312c7cc
lucifer_dream <422512433@qq.com>
2016-05-29 23:45:42 +0800

    first_commit

---
+++ src/calculate-median.js
@@ -0,0 +1,22 @@
+﻿function calculate_median(arr) {
+function sum(a(),n)//传入n值
+{
+var i;A=0;
+for(i=0;i<n;i++)
+if(i%2==0)
+A+=a(i);
+return A;
+}
+int main(){
+var i,B=0,n;
+new a(1000);
+n=prompt();
+for(i=0;i<n;i++)
+a(i)=prompt();
+B=function sum(a,n);
+document.write(B);
+return 0;
+}
+}
+
+module.exports = calculate_median;
