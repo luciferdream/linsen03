8be129d388e41d4bd94b40268c018ce68c060f12
luciferdream <422512433@qq.com>
2016-05-31 14:25:52 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,11 +1,11 @@
 function calculate_median(arr) {
 	var tempArr = new Array();
 	var k = 0;
+	arr=arr.sort();
 	for(var i = 0; i < arr.length; i++){
 		if(i%2 == 0)
 			tempArr = arr[i];
 	}
-	tempArr = tempArr.sort();
 	if(tempArr.length%2 == 0) return tempArr
 
 [tempArr.length/2];
